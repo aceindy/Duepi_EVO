4e116f24beb5b1a14ac27939c29502a4df7e4b78
Atze de Vries <atze.de.vries@km.kongsberg.com>
2020-12-28 21:42:26 +0100

    Added more return values to const

--- evo-python/const.py
+++ evo-python/const.py
@@ -1,18 +1,20 @@
-#constants
+# constants
 
-#Status results
-str_cool1="0802002A"
-str_cool2="08010029"
-str_off="00000020"
-str_igniting="01010022"
-str_ignited="02010023"
-str_ack="00000020"
+# Status results
+str_off = "00000020"       # Off
+str_igniting = "01010022"  # Ignition starting
+str_ignited = "02010023"   # Flame on
+str_cool1 = "0802002A"     # Cooling down
+str_cool2 = "08010029"     # Cooling down
+str_cool3 = "10010022"     # Cooling down (eco)
+str_eco_off = "10030024"   # Eco standby
+str_ack = "00000020"       # both acknoledge and off
 
-#Get data
+# Get data
 get_status = "\x1bRD90005f&"
 get_temperature = "\x1bRD100057&"
 
-#Set data
+# Set data
 set_temperature = "\x1bRF2xx0yy&"
 set_powerLevel = "\x1bRF00x0yy&"
 set_powerOff = "\x1bRF000058&"
