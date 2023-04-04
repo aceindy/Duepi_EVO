1ace2f2e5af5972513c421d869af524c8d067950
aceindy <aceindy@gmail.com>
2023-04-04 11:15:22 +0200

    Update const.py
    Sorted and added some vars

--- evo-python/const.py
+++ evo-python/const.py
@@ -1,10 +1,15 @@
 # constants
-state_ack =    0x00000020
-state_off =    0x00000020
-state_start =  0x01000000
-state_on =     0x02000000
-state_cool =   0x08000000
-state_eco =    0x10000000
+state_ack   = 0x00000020
+state_eco   = 0x10000000
+state_clean = 0x04000000
+state_cool  = 0x08000000
+state_off   = 0x00000020
+state_on    = 0x02000000
+state_start = 0x01000000
+
+
+remote_reset    = "\x1bRD60005C&"
+
 
 # Status results
 str_off =      "00000020"              # Off
@@ -20,20 +25,20 @@ str_eco_off =  "10030024"              # Eco standby
 str_ack =      "00000020"              # both acknoledge and off
 
 # Get data
-get_status =      "\x1bRD90005f&"
-
-get_setpoint =    "\x1bRC60005B&"
-
+get_errorstate  = "\x1bRDA00067&"
+get_exhfanspeed = "\x1bREF0006D&"
+get_flugastemp  = "\x1bRD000056&" 
+get_pelletspeed = "\x1bRD40005A&"
+get_setpoint    = "\x1bRC60005B&"
+get_status      = "\x1bRD90005f&"
 get_temperature = "\x1bRD100057&"
-set_temperature = "\x1bRF2xx0yy&"
-
-get_setpoint = "\x1bRC60005B&"
+get_powerLevel  = "\x1bRD300059&"
 
 # Set data
+set_augercor    = "\x1bRD50005A&"
+set_extractcor  = "\x1bRD50005B&"
 set_temperature = "\x1bRF2xx0yy&"
-
-set_powerLevel =  "\x1bRF00x0yy&"
-set_powerOn =     "\x1bRF001059&"
-
-set_powerOff =    "\x1bRF000058&"
-set_powerOn =     "\x1bRF001059&"
+set_pelletcor   = "\x1bRD50005B&"
+set_powerLevel  = "\x1bRF00xx0yy&"
+set_powerOff    = "\x1bRF000058&"
+set_powerOn     = "\x1bRF001059&"
