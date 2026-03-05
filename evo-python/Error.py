148c463a81fca6caca479d9231f37b6cf345c77c
aceindy <aceindy@gmail.com>
2026-03-05 11:53:07 +0100

    Did threw an error on github
    language:python
    Status
    1 warning
    
    Could not process some files due to syntax errors
    
    A parse error occurred while processing /home/runner/work/Duepi_EVO/Duepi_EVO/evo-python/Error.py, and as a result this file could not be analyzed. Check the syntax of the file using the python -m py_compile command and correct any invalid syntax.

--- evo-python/Error.py
+++ evo-python/Error.py
@@ -1,6 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-sock.connect(("adresseIP", port#))
+sock.connect(("192.168.103.11", 23))
 cmdGetStatus = "\x1bRDA00067&"
 sock.send(cmdGetStatus.encode())
 dataFromServer = sock.recv(10);
@@ -34,4 +34,5 @@ elif statusInt == 14 :
   print("Surchauffe")
 else:
   print(statusInt)
+
 sock.close()
